3b9b2a34653f8f7992f289857e975062e87e7d21
kaiyga <kaiyga@proton.me>
2025-05-17 13:09:20 +0500

    Nvim Init

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,45 @@
+local linters = {
+  'gopls',
+  'lua_ls',
+  'pyright',
+  'solargraph',
+  'html',
+}
+
+return {
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function() require("mason").setup() end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      local lspconfig = require "lspconfig"
+      -- Install linters in list
+      for _, s in ipairs(linters) do
+        local server = lspconfig[s]
+        if server and server.setup then
+          server.setup {
+            capabilities = capabilities,
+          }
+        end
+      end
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
+}
---
+++ lua/vim-options.lua
@@ -0,0 +1,24 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+vim.g.background = "light"
+
+vim.opt.swapfile = false
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.wo.number = true
+
+vim.keymap.set({"n", "i"}, "<C-f>", function()
+    vim.cmd.stopinsert()
+    vim.lsp.buf.signature_help()
+    vim.defer_fn(function() vim.cmd.wincmd("w") end, 100)
+    -- vim.keymap.set("n", "q", ":close<CR>", { buffer = 0 })
+end)
